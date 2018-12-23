6ea3a95ad15df35b860d3d7f34fedd21028cbf5e
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-12-23 03:03:44 -0300

    Add Slide to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,97 @@
 import React, { Component } from 'react';
+import { Image, Row, Col, Grid } from 'react-bootstrap';
+import { CarouselItem, CarouselCaption, CarouselIndicators, CarouselControl, Carousel } from 'reactstrap';
+
+const items = [
+    {
+        src: '/caroussel/1.jpg',
+        altText: 'Slide 1',
+        caption: 'Slide 1'
+    },
+    {
+        src: '/caroussel/2.jpg',
+        altText: 'Slide 1',
+        caption: 'Slide 1'
+    },
+    {
+        src: '/caroussel/3.jpg',
+        altText: 'Slide 1',
+        caption: 'Slide 1'
+    }
+];
 
 
 
 class Home extends Component {
-  render() {
-    return (
-     <div>Home</div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = { activeIndex: 0 };
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.goToIndex = this.goToIndex.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
+    }
+
+    onExiting() {
+        this.animating = true;
+    }
+
+    onExited() {
+        this.animating = false;
+    }
+
+    next() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({ activeIndex: nextIndex });
+    }
+
+    previous() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+        this.setState({ activeIndex: nextIndex });
+    }
+
+    goToIndex(newIndex) {
+        if (this.animating) return;
+        this.setState({ activeIndex: newIndex });
+    }
+
+    render() {
+        const { activeIndex } = this.state;
+
+        const slides = items.map((item) => {
+            return (
+                <CarouselItem
+                    onExiting={this.onExiting}
+                    onExited={this.onExited}
+                    key={item.src}
+                >
+                    <img  width={900} height={500} src={item.src} alt={item.altText} />
+                    <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+                </CarouselItem>
+            );
+        });
+        return (
+            <Grid>
+
+                <Carousel
+                    activeIndex={activeIndex}
+                    next={this.next}
+                    previous={this.previous}
+                >
+                    <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                    {slides}
+                    <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                    <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+                </Carousel>
+
+
+            </Grid>
+        );
+    }
 }
 
 export default Home;
