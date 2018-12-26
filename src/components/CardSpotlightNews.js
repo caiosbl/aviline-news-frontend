682b0ffd96ae3fb888d1123a2dfabd15ffcec715
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-12-26 02:06:58 -0300

    Estilo do Card Redefinido

--- src/components/CardSpotlightNews.js
+++ src/components/CardSpotlightNews.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Card,CardImg,CardImgOverlay,CardTitle,CardText} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, Badge, CardText } from 'reactstrap';
 import Spinner from './Spinner';
 import renderHTML from 'react-render-html';
 import { Link } from 'react-router-dom';
@@ -9,7 +9,7 @@ import '../styles/CardSpotlightNews.css';
 
 class CardSpotlightNews extends Component {
 
-    
+
     constructor(props) {
         super(props);
         this.state = {
@@ -30,8 +30,8 @@ class CardSpotlightNews extends Component {
             })
             .then(function (res) {
                 res.length < that.state.position ? that.setState({ notFound: true, load: false }) :
-                
-                that.setState({data : res[that.state.position - 1], load: false });
+
+                    that.setState({ data: res[that.state.position - 1], load: false });
             });
 
     }
@@ -39,24 +39,30 @@ class CardSpotlightNews extends Component {
     render() {
 
         this.state.load &&
-        this.getData(this);
+            this.getData(this);
 
         const data = this.state.data;
 
-     return(
+        return (
 
-        (this.state.load || this.state.notFound) ? <Spinner/> : 
-        <Link to={"/news/" + data.slug} id="link"> 
-       <Card inverse id="Card">
-        <CardImg width="100%" src={data.image.url} 
-        alt="Card image cap" style={{opacity: 0.5}}/>
-        <CardImgOverlay>
-         <CardText id='CardTitle'><h3>{data.title}</h3></CardText>
-       
-         
-        </CardImgOverlay>
-    </Card>
-    </Link>);
+            (this.state.load || this.state.notFound) ? <Spinner /> :
+                <Link to={"/news/" + data.slug} id="link">
+                    <Card inverse id="Card">
+
+
+
+                        <CardImg width="100%" src={data.image.url}
+                            alt="Card image cap" style={{ opacity: 0.5 }} />
+                        <CardImgOverlay>
+
+                            {data.categories[0] !== undefined &&
+                                <CardText>
+                                    <Badge color="danger" id="badge"><h5>{data.categories[0].name}</h5></Badge>
+                                </CardText>}
+                            <CardText id='CardTitle'><h3>{data.title}</h3></CardText>
+                        </CardImgOverlay>
+                    </Card>
+                </Link>);
     }
 }
 
