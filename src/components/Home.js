43c7f7be1a59ecdf01839cdb84b37fe82de2545b
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-12-28 13:50:44 -0300

    Add Spinner em Home quando a promise de Data não estiver cumprida

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { } from 'reactstrap';
 import LatestNews from './LatestNews';
 import CardSpotlightNews from './CardSpotlightNews'
 import MaterialIcon from 'material-icons-react';
+import Spinner from './Spinner';
 import CardSecondaryNews from './CardSecondaryNews';
 import CardQuotation from './CardQuotation';
 import '../styles/Home.css';
@@ -15,7 +16,8 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: ''
+            data: '',
+            load: true,
         };
     }
 
@@ -26,28 +28,34 @@ class Home extends Component {
                 return response.json();
             })
             .then(function (res) {
-                that.setState({ data: res, isLoading: false });
+                res.length == 0 ? that.setState({ notFound: true, load: false }) :
+                that.setState({ data: res, load: false });
             });
 
     }
     
 
     render() {
+
+        this.state.load && this.getData(this);
+
         return (
             <Grid>
 
                 <Row>
                     <Col md={4} mdPull={4} style={{marginBottom: 10}}>
-                        <CardSpotlightNews position={1} />
-
+                    {this.state.load || this.state.notFound ? <Spinner /> :
+                        <CardSpotlightNews position={1} />}
                     </Col>
 
                     <Col md={4} mdPull={6} style={{marginBottom: 10}}>
-                        <CardSpotlightNews position={2} />
+                    {this.state.load || this.state.notFound ? <Spinner /> :
+                        <CardSpotlightNews position={2} />}
                     </Col>
 
                     <Col md={4} mdPull={4} style={{marginBottom: 10}}>
-                        <CardSpotlightNews position={3} />
+                    {this.state.load || this.state.notFound ? <Spinner /> :
+                        <CardSpotlightNews position={3} />}
                     </Col>
 
 
@@ -57,24 +65,27 @@ class Home extends Component {
                 <Row>
 
                 <Col md={2} mdPull={2} style={{marginBottom: 10}}>
-                        <CardSecondaryNews position={1} />
-                        <CardSecondaryNews position={2} />
+                {this.state.load || this.state.notFound ? <Spinner /> :
+                        <div><CardSecondaryNews position={1} />
+                        <CardSecondaryNews position={2} /></div>}
                     </Col>
                     <Col md={2} mdPull={2} style={{marginBottom: 10}}>
+                    {this.state.load || this.state.notFound ? <Spinner /> : <div>
                         <CardSecondaryNews position={2} />
-                        <CardSecondaryNews position={1} />
+                        <CardSecondaryNews position={1} /></div>}
                     </Col>
                     <Col md={2} mdPull={2} style={{marginBottom: 10}}>
+                    {this.state.load || this.state.notFound ? <Spinner /> : <div>
                         <CardSecondaryNews position={3} />
-                        <CardSecondaryNews position={1} />
+                        <CardSecondaryNews position={1} /></div>}
                     </Col>
                     <Col md={2} mdPull={2} style={{marginBottom: 10}}>
+                    {this.state.load || this.state.notFound ? <Spinner /> : <div>
                         <CardSecondaryNews position={4} />
-                        <CardSecondaryNews position={1} />
+                        <CardSecondaryNews position={1} /></div>}
                     </Col>
                     <Col md={4} mdPull={2} style={{marginBottom: 10}}>
-                        <CardQuotation/>
-                        
+                        <CardQuotation/>    
                     </Col>
     
                 </Row>
@@ -82,7 +93,8 @@ class Home extends Component {
                 <Row style={{marginTop: 10}}>
 
                     <Col md={8} mdPull={4} style={{marginBottom: 40}}>
-                                <LatestNews />
+                    {this.state.load || this.state.notFound ? <Spinner /> : 
+                    <LatestNews />}
                     </Col>
 
                 </Row>
