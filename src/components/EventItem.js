3de0882b49e719b10669624a6da1378a310de17e
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-12-31 14:32:34 -0300

    Estilo de Event Item atualizado

--- src/components/EventItem.js
+++ src/components/EventItem.js
@@ -11,7 +11,7 @@ import 'moment/locale/pt-br';
 class EventItem extends Component {
 
     constructor(props) {
-        super(props);  
+        super(props);
     }
 
 
@@ -24,13 +24,56 @@ class EventItem extends Component {
 
         return (
             <Card style={{ margin: 20 }}>
-                <CardHeader style={{ backgroundColor: '#CC0000', color: 'white', fontFamily: 'Roboto Condensed, sans-serif' }}>
+                <CardHeader style={{ backgroundColor: "#dc3545", color: 'white', fontFamily: 'Roboto Condensed, sans-serif' }}>
                     <h4><b>{event.title}</b></h4>
                 </CardHeader>
 
                 <CardBody>
                     <Grid>
                         <Row>
+
+                            <h5
+                                style={{
+                                    fontFamily: 'Roboto Condensed, sans-serif',
+                                    color: "#dc3545"
+                                }}
+                            >
+                                <span style={{ marginRight: 4 }}>
+                                    <MaterialIcon icon="event_note" color="#dc3545" size={16} /></span>
+
+                                De {<Moment
+                                    locale="pt-br"
+                                    format="D MMMM" withTitle
+                                >
+
+                                    {event.dateStart}</Moment>}  à  {<Moment
+                                        locale="pt-br"
+                                        format="D MMMM" withTitle
+                                    >{event.dateFinish}</Moment>} de {<Moment
+                                        locale="pt-br"
+                                        format="YYYY" withTitle
+                                    >{event.dateFinish}</Moment>}
+
+                            </h5>
+
+                            </Row>
+                            <Row>
+
+                            <h5
+                                style={{ fontFamily: 'Roboto Condensed, sans-serif', color: "#dc3545" }}
+                            > <span style={{ marginRight: 4 }}>
+                                    <MaterialIcon icon="location_on" color="#dc3545" size={16} /></span>
+                                {event.location}
+
+                            </h5>
+
+
+                        </Row>
+
+
+
+                        <Row>
+
                             {hasImage && <Col xs={6} md={4} >
 
                                 <Image src={event.image.url} thumbnail />
@@ -38,43 +81,14 @@ class EventItem extends Component {
                             </Col>
 
                             }
-
-                            <Col xs={12} md={mdDataCol} >
-
-                                <h5
-                                    style={{ fontFamily: 'Roboto Condensed, sans-serif' }}
-                                >
-                                    <span style={{ marginRight: 4 }}><MaterialIcon icon="event_note" color="black" size={16} /></span>
-
-                                    De {<Moment
-                                        locale="pt-br"
-                                        format="D MMMM" withTitle
-                                    >
-
-                                        {event.dateStart}</Moment>}  à  {<Moment
-                                            locale="pt-br"
-                                            format="D MMMM" withTitle
-                                        >{event.dateFinish}</Moment>} de {<Moment
-                                            locale="pt-br"
-                                            format="YYYY" withTitle
-                                        >{event.dateFinish}</Moment>}
-
-                                </h5>
-
-                                <h5
-                                 style={{ fontFamily: 'Roboto Condensed, sans-serif' }}
-                                > <span style={{ marginRight: 4 }}>
-                                    <MaterialIcon icon="location_on" color="black" size={16} /></span>
-                                    {event.location}
-
-                                </h5>
-
+                            <Col xs={12} md={12}>
                                 <Link to={`/events/${event.slug}`}> <Button color="danger"
-                                 style={{ fontFamily: 'Roboto Condensed, sans-serif' }}
-                                >
-                                
-                                Ver Mais
-                                
+                                    style={{
+                                        fontFamily: 'Roboto Condensed, sans-serif',
+                                        width: '100%',
+                                        marginTop: 10
+                                    }}>
+                                    Ver Mais
                                 </Button></Link>
 
                             </Col>
