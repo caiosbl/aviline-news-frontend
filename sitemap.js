e099dfe5d6e61c3c177033b7b8a52241bba66cab
caiosbl <caio.lira@ccc.ufcg.edu.br>
2019-01-05 13:47:41 -0300

    Sitemap updated

--- sitemap.js
+++ sitemap.js
@@ -2,23 +2,100 @@ var express = require('express')
 var sm = require('sitemap');
 
 var app = express();
-var sitemap = sm.createSitemap ({
-    hostname: 'http://example.com',
+var sitemap = sm.createSitemap({
+    hostname: 'http://aviline.com.br',
     cacheTime: 600000
-  });
+});
 
-sitemap.add({url: '/'});
-sitemap.add({url: '/events'});
-sitemap.add({url: '/categories'});
-
-
+sitemap.add({ url: '/' });
+sitemap.add({ url: '/events' });
+sitemap.add({ url: '/categories' });
 
 
+try{
+fetch("https://aviline.herokuapp.com/api/post")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (res) {
 
+        res.forEach(news => {
+            sitemap.add({ url: `/news/${slug}` });
+        });
+       
+    });
+}
+catch(e){}
+
+try{
+    fetch("https://aviline.herokuapp.com/api/category")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (res) {
     
-app.get('/sitemap.xml', function(req, res) {
-  res.header('Content-Type', 'application/xml');
-  res.send( sitemap.toString() );
+            res.forEach(news => {
+                sitemap.add({ url: `/categories/${name}` });
+            });
+           
+        });
+    }
+    catch(e){}
+
+
+
+try{
+    fetch(`https://aviline.herokuapp.com/api/event`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (res) {
+    
+            res.forEach(news => {
+                sitemap.add({ url: `/events/${slug}` });
+            });
+           
+        });
+    }
+    catch(e){}
+
+
+    try{
+        fetch(`https://aviline.herokuapp.com/api/column-author`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (res) {
+        
+                res.forEach(news => {
+                    sitemap.add({ url: `/columns/${slug}` });
+                });
+               
+            });
+        }
+        catch(e){} 
+    
+
+try{
+    fetch(`https://aviline.herokuapp.com/api/column`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (res) {
+    
+            res.forEach(news => {
+                sitemap.add({ url: `/column/${slug}` });
+            });
+           
+        });
+    }
+    catch(e){}
+
+
+
+app.get('/sitemap.xml', function (req, res) {
+    res.header('Content-Type', 'application/xml');
+    res.send(sitemap.toString());
 });
 
 app.listen(3000);
