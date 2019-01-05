68096830526b56e81292e2ed873e0515afa9957f
caiosbl <caio.lira@ccc.ufcg.edu.br>
2019-01-05 17:40:42 -0300

    Add Build

---
+++ build/precache-manifest.06b4baae71b3be5a39d108e19be045dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "260fc52aa042cda3ef91",
+    "url": "/static/css/main.306f80ef.chunk.css"
+  },
+  {
+    "revision": "260fc52aa042cda3ef91",
+    "url": "/static/js/main.260fc52a.chunk.js"
+  },
+  {
+    "revision": "a30a2d7214396d332287",
+    "url": "/static/css/1.616b685d.chunk.css"
+  },
+  {
+    "revision": "a30a2d7214396d332287",
+    "url": "/static/js/1.a30a2d72.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c278df3caeca4c031bf417277c7d00e",
+    "url": "/index.html"
+  }
+];
